24a8a71b2b77a2c63567095ffa7c187f7d239c49
Pinkey Rani <pinkeyrani894@gmail.com>
2025-08-04 21:54:32 +0600

    javascript assignment

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const header = document.querySelectorAll(".header");
+const details = document.querySelectorAll(".details");
+
+header.forEach((head, ind) => {
+    head.addEventListener("click", () => {
+        details.forEach((d, i) => {
+            if (ind == i) {
+                details[ind].style.display = details[ind].style.display == "block" ? "none" : "block";
+            }
+            else {
+                details[i].style.display = "none";
+            }
+        })
+
+    })
+
+})
+// header[0].addEventListener("click", () => {
+// details[0].style.display = details[0].style.display == "block" ? "none": "block";
+// })
+
+// header.forEach((head, ind) => {
+//     head.addEventListener("click" ,() => {
+//         details[ind].style.display = details[0].style.display == "block" ? "none": "block";
+
+//     })
+// })
+const bttn = document.querySelectorAll(".bttn");
+const sent = document.querySelectorAll(".sent");
+
+bttn.forEach((headd, nd) => {
+ headd.addEventListener("click",() => {
+
+    sent.forEach((dd, s) => {
+        if(nd == s){
+            sent[nd].style.display = sent[nd].style.display == "block" ? "none": "block";
+        }
+        else{
+            sent[s].style.display ="none";
+        }
+    })
+    })
+})
+
