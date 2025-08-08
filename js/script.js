a2d54ae3573ed18e70977da31618423734b8094c
Pinkey Rani <pinkeyrani894@gmail.com>
2025-08-08 22:01:06 +0600

    question no 1

--- js/script.js
+++ js/script.js
@@ -15,30 +15,55 @@ header.forEach((head, ind) => {
     })
 
 })
-// header[0].addEventListener("click", () => {
-// details[0].style.display = details[0].style.display == "block" ? "none": "block";
-// })
 
-// header.forEach((head, ind) => {
-//     head.addEventListener("click" ,() => {
-//         details[ind].style.display = details[0].style.display == "block" ? "none": "block";
-
-//     })
-// })
 const bttn = document.querySelectorAll(".bttn");
 const sent = document.querySelectorAll(".sent");
 sent[0].style.display ="block" ;
+bttn[0].style.backgroundColor = "#ede435ff"; 
+
 bttn.forEach((headd, nd) => {
  headd.addEventListener("click",() => {
 
     sent.forEach((dd, s) => {
         if(nd == s){
              sent[nd].style.display ="block" ;
+             bttn[nd].style.backgroundColor = "#ede435ff"; 
+
         }
         else{
             sent[s].style.display ="none";
+            bttn[s].style.backgroundColor = "#bef576"; 
+
         }
     })
     })
 })
+const hour = document.querySelectorAll(".hrs12")
+const time = document.getElementById("time");
+const showTime = () =>{
+const d = new Date();
+let hrs = d.getHours();
+const ampm = hrs >= 12 ? "PM":"AM";
+ hrs = hrs > 12? hrs-12 :hrs ;
 
+// hour.forEach((hours, index) => {
+// hours.addEventListener("click",()=>{
+
+// hour.[index] = hrs > 12? hrs-12 :hrs ;})
+// })
+
+time.innerText = hrs + ":"+ d.getMinutes() + ":" + d.getSeconds() +" "+ ampm;
+console.log(d.getHours()+ ":"+ d.getMinutes() + ":" + d.getSeconds() )
+}
+showTime();
+setInterval(showTime, 1000);
+
+
+// hours.forEach( (time, ss)=>{
+//     if(index=ss){
+//         hrs = hrs > 12? hrs-12 :hrs ;
+//     }
+//     else{
+//         hrs = hrs;
+//     }
+// })
